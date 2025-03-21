bb4c43d40cbcb900e2541c9b7823b6680bd3767c
Jing-Tang <jtchen.ntu@gmail.com>
2025-03-21 22:52:43 +0800

    easy fourth.

--- student_agent.py
+++ student_agent.py
@@ -4,10 +4,13 @@ import pickle
 import random
 import gym
 
-last_step = 0
+last_passenger = 0
+last_destination = 0
 
 def get_action(obs):
-    global last_step
+    global last_passenger
+    global last_destination
+    
     stations = [[0,0] for _ in range(4)]
     taxi_row, taxi_col, stations[0][0], stations[0][1] , stations[1][0], stations[1][1], stations[2][0], stations[2][1], stations[3][0], stations[3][1], obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look = obs
 
@@ -21,11 +24,13 @@ def get_action(obs):
     if not obstacle_west:
         actions.append(3)
     
-    if [taxi_row, taxi_col] in stations and last_step:
+    if [taxi_row, taxi_col] in stations and last_passenger:
         actions.append(4)
+    if [taxi_row, taxi_col] in stations and last_destination:
         actions.append(5)
         
-    last_step = passenger_look or destination_look
+    last_passenger = passenger_look
+    last_destination = destination_look
     if not actions: # just in case
         actions = [0,1,2,3,4,5]
     return random.choice(actions) # Choose a random action
